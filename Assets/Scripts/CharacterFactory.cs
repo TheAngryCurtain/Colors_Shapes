34d0234ab26616defbd32be0217e7d8b36f5329b
Mike <mike.d.samson@gmail.com>
2016-08-12 23:59:56 -0300

    New Level work
    Added new prefabs related to the village, and started the re-design for
    the world map. Expanded terrain, started village and mountain.

---
+++ Assets/Scripts/CharacterFactory.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFactory : MonoBehaviour
+{
+    public enum Mob { Rabbit = 0, Bird, Wolf, Bear };
+    public enum Boss { Bee = 0 };
+
+    [SerializeField]
+    private GameObject playerPrefab;
+
+    [SerializeField]
+    private GameObject[] mobPrefabs;
+
+    [SerializeField]
+    private GameObject[] bossPrefabs;
+
+    void Start()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    // IDEAS
+    // pso-style - each level can have different configurations which change up the level each time to you play
+    // have a varity of missions all taking place in the sa me level
+    //    > Find and speak to NPC, recover specific treasure, kill all enemies, etc
+    //    > save character progression and scale difficulty of each randomly chosen mission based on level?
+    //    > ... will probably need to make the level bigger...
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+       CharacterFactory factory = CreateCharacterFactory();
+
+        // TODO
+        // generate and pick a level configuration
+        //    > choose player spawn, then pick enemy spawns and boss spawn
+        // instantiate/position player
+    }
+
+    private CharacterFactory CreateCharacterFactory()
+    {
+        GameObject f = new GameObject();
+        f.name = "Character Factory";
+        return f.AddComponent<CharacterFactory>();
+    }
+}
