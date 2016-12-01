0b8285e3bf8dd49dab2853eb4534d6f5fcef7ac3
Mike <mike.d.samson@gmail.com>
2016-11-30 23:07:36 -0400

    Threw in unadapted camera controller

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    private const float MIN_Y_ANGLE = 0f;
+    private const float MAX_Y_ANGLE = 50f;
+
+    [SerializeField] private Transform _target;
+    [SerializeField] private Transform _camTransform;
+    [SerializeField] private Camera _camera;
+
+    private float distance = 10f;
+    private float currentX = 0f;
+    private float currentY = 0f;
+    private float sensitivityX = 4f;
+    private float sensitivityY = 2f;
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Horizontal_RS") * sensitivityX;
+        currentY += Input.GetAxis("Vertical_RS") * sensitivityY;
+
+        currentY = Mathf.Clamp(currentY, MIN_Y_ANGLE, MAX_Y_ANGLE);
+    }
+
+    private void LateUpdate()
+    {
+        if (_target != null)
+        {
+            Vector3 dir = new Vector3(0f, 0f, -distance);
+            Quaternion rotation = Quaternion.Euler(currentY, currentX, 0f);
+            _camTransform.position = _target.position + rotation * dir;
+            _camTransform.LookAt(_target.position);
+        }
+    }
+}
